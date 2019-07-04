eccc55d0550dbc0cbcf9d50574093acf81a7686e
Sebastien <seb75@fedoraproject.org>
2019-07-04 15:41:19 +0200

    repaired phpunit after missed manipulation

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,4 +13,4 @@ require('../css/app.scss');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+//console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
