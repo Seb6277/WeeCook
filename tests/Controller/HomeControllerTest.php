e2c59ce839b7b6171d146356c80e67b322edaec0
Sebastien <seb75@fedoraproject.org>
2019-07-05 12:04:06 +0200

    test implementation for signup and signin

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -26,4 +26,28 @@ class HomeControllerTest extends WebTestCase
 
         $this->assertContains('nav', $this->client->getResponse()->getContent());
     }
+
+    public function testSigninPageIsUp() {
+        $this->client->request('GET', '/signin');
+
+        static::assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testSigninPageIsRenderingCorrectly() {
+        $this->client->request('GET', '/signin');
+
+        $this->assertContains('h2', $this->client->getResponse()->getContent());
+    }
+
+    public function testSignupPageIsUp() {
+        $this->client->request('GET', '/signup');
+
+        static::assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testSignupPageIsRenderingCorrectly() {
+        $this->client->request('GET', '/signup');
+
+        $this->assertContains('h2', $this->client->getResponse()->getContent());
+    }
 }
