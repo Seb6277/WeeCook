fed9df512d833289d4bba78634244c312f59d3a0
Sebastien <seb75@fedoraproject.org>
2019-07-12 11:11:18 +0200

    added dataFixtures for Ingredient

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Ingredient;
+use App\Entity\IngredientQuantity;
+use App\Entity\Recipe;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RecipeController extends AbstractController
@@ -35,8 +40,29 @@ class RecipeController extends AbstractController
      * @return \Symfony\Component\HttpFoundation\Response
      * @Route("/create", name="creation_page")
      */
-    public function editRecipe()
+    public function editRecipe(Request $request): Response
     {
+        // Test if the form is submitted with the length of the request
+        // If request length = 0 the method was GET so the publish button was not clicked
+        if (!$request->request->count() == 0)
+        {
+            //Create a new empty recipe
+            $recipe = new Recipe();
+            $ingredient = new Ingredient();
+            $ingredient_quantity = new IngredientQuantity();
+            // Retrieve the entire request array
+            $data = $request->request;
+            // Place different parts into object
+            $recipe->setName($data->get('recipe_name'));
+            $recipe->setPreparation($data->get('preparation'));
+            //
+            $ingredient->setName($data->get('ingredient0'));
+            $ingredient_quantity->setQuantity($data->get('quantity0'));
+            dump($recipe);
+            dump($ingredient);
+            dump($ingredient_quantity);
+        }
+
         return $this->render('recipe/create.html.twig', [
             'nbrIngredients' => 6
         ]);
