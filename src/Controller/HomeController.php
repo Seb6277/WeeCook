1e4e4a6378736eaf1fc216753ef61ba1910ca289
Sebastien <seb75@fedoraproject.org>
2019-07-24 10:48:19 +0200

    finished image recipe on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
 use App\Interfaces\HomeControllerInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -10,6 +13,28 @@ use Twig\Environment;
 
 class HomeController implements HomeControllerInterface
 {
+    /**
+     * @var ObjectManager
+     */
+    private $manager;
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * HomeController constructor.
+     * @param ObjectManager $manager
+     */
+    public function __construct(
+        ObjectManager $manager,
+        ContainerInterface $container
+    ){
+        $this->manager = $manager;
+        $this->container = $container;
+    }
+
     /**
      * @Route("/", name="home", methods={"GET"})
      *
@@ -21,8 +46,24 @@ class HomeController implements HomeControllerInterface
      */
     public function __invoke(Environment $twig):Response
     {
+        $repository = $this->manager->getRepository(Recipe::class);
+        $recipes = $repository->getThreeLatest();
+
         return new Response($twig->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'recipe_image_1' => $this->getImageUri($recipes, 0),
+            'recipe_image_2' => $this->getImageUri($recipes, 1),
+            'recipe_image_3' => $this->getImageUri($recipes, 2),
         ]));
+    }
+
+    /**
+     * @param array $recipes
+     * @param int $index
+     * @return string
+     */
+    private function getImageUri(array $recipes, int $index):string
+    {
+        return '/uploads/images/'.$recipes[$index]->getImage1();
     }
 }
--- src/Interfaces/HomeControllerInterface.php
+++ src/Interfaces/HomeControllerInterface.php
@@ -4,10 +4,17 @@
 namespace App\Interfaces;
 
 
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 
 interface HomeControllerInterface
 {
+    public function __construct(
+        ObjectManager $manager,
+        ContainerInterface $container
+    );
+
     public function __invoke(Environment $twig):Response;
 }
