8f42e73bf8037073cd3c8ca6ffa4755da8205720
Sebastien <seb75@fedoraproject.org>
2019-08-17 12:07:39 +0200

    begin moderation system

---
+++ src/Form/ModerationType.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created with PHPStorm
+ * Date: 17/8/2019
+ * Time: 10:25
+ * Author: S. Carpentier
+ * Mail: sebastien.carpentier89@gmail.com
+ */
+
+namespace App\Form;
+
+
+class ModerationType
+{
+
+}
