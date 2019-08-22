fdca1fbcb1734214145a4e981c73f74be16f1cf5
Sebastien <seb75@fedoraproject.org>
2019-08-22 14:52:30 +0200

    added favorite feature

--- src/Controller/RecipeShowController.php
+++ src/Controller/RecipeShowController.php
@@ -10,14 +10,18 @@
 namespace App\Controller;
 
 use App\Controller\Interfaces\RecipeShowControllerInterface;
+use App\Entity\Favorite;
 use App\Entity\Ingredient;
 use App\Entity\IngredientQuantity;
 use App\Entity\Recipe;
+use App\Entity\User;
 use App\Utils\RecipeUtils;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Twig\Environment;
 
 class RecipeShowController implements RecipeShowControllerInterface
@@ -28,22 +32,35 @@ class RecipeShowController implements RecipeShowControllerInterface
     private $manager;
 
     /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    /**
      * RecipeShowController constructor.
      * @param ObjectManager $manager
      */
-    public function __construct(ObjectManager $manager)
+    public function __construct(SessionInterface $session, ObjectManager $manager, TokenStorageInterface $tokenStorage)
     {
         $this->manager = $manager;
+        $this->tokenStorage = $tokenStorage;
+        $this->session = $session;
     }
 
     /**
-     * @Route("/show/{id}", name="recipe_show", methods={"GET"}, requirements={"id" = "\d+"})
+     * @Route("/show/{id}", name="recipe_show", methods={"GET", "POST"}, requirements={"id" = "\d+"})
      *
      * @param Environment $twig
      * @return string
      * @throws \Twig\Error\LoaderError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
+     * @throws \Doctrine\ORM\NonUniqueResultException
      */
     public function __invoke(Request $request, Environment $twig, int $id):Response
     {
@@ -58,9 +75,12 @@ class RecipeShowController implements RecipeShowControllerInterface
             ->getRepository(IngredientQuantity::class);
         $ingredientRepository = $this->manager
             ->getRepository(Ingredient::class);
+        $favoriteRepository = $this->manager
+            ->getRepository(Favorite::class);
 
         // Retrieve recipe information for id
         $recipe = $recipeRepository->find($id);
+        $user = $this->tokenStorage->getToken()->getUser();
 
         // Retrieve list of ingredient in a array
         $ingredientQuantity = $ingredientQuantityRepository->getAllItemsByRecipe($id);
@@ -76,6 +96,36 @@ class RecipeShowController implements RecipeShowControllerInterface
             array_push($measures, $ingredientRepository->find($ingredient)->getMesureUnit());
             array_push($quantities, $quantity);
         }
+
+        $existingFavorite = $favoriteRepository
+            ->getFavoriteExist($user, $recipe);
+
+        $existingFavorite === null ? $isFavorite = false : $isFavorite = true;
+
+        if ($request->getMethod() === "POST")
+        {
+            if ($existingFavorite === null)
+            {
+                $favorite = new Favorite();
+                $favorite->setUser($user);
+                $favorite->setRecipe($recipe);
+
+                $this->manager->persist($favorite);
+                $this->manager->flush();
+
+                $isFavorite = true;
+
+                $this->session->getFlashBag()->add('info', 'Recette ajouter au favoris');
+            } else {
+                $this->manager->remove($existingFavorite);
+
+                $this->manager->flush();
+
+                $isFavorite = false;
+
+                $this->session->getFlashBag()->add('info', 'Recette retirer des favoris');
+            }
+        }
         
         return new Response($twig->render('recipe/show.html.twig', [
             'controller_name' => 'RecipeShowController',
@@ -85,7 +135,8 @@ class RecipeShowController implements RecipeShowControllerInterface
             'quantities' => $quantities,
             'measures' => $measures,
             'ingredient_length' => count($ingredients),
-            'image1' => RecipeUtils::getImageUri($recipe)
+            'image1' => RecipeUtils::getImageUri($recipe),
+            'is_favorite' => $isFavorite
         ]));
     }
 }
